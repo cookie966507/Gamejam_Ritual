6cb2de2c5c62fb8b9962859ee633c90981598dfd
Eric Cook <jcook83@gatech.edu>
2016-01-30 00:43:50 -0500

    Rolling like a not square

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,7 +6,30 @@ namespace Assets.Scripts.Player
 	public class Movement : ControllerObject {
 
 		private bool facingRight = true;
+        private bool rolling = false;
+        private float rollTimer = 0, rollTime = 0.3f;
         private float moveSpeed = 5f;
+        private float rollSpeed = 20f, rollSlerp = 20f, targetSlerp = 5f, slerpSpeed = 0.1f;
+        private float rollVel = 0f;
+
+        void Update()
+        {
+            if(rolling)
+            {
+                rollTimer += Time.deltaTime;
+                rollSlerp = Mathf.SmoothDamp(rollSlerp, targetSlerp, ref rollVel, slerpSpeed);
+                if(rollTimer >= rollTime)
+                {
+                    rollTimer = 0;
+                    rolling = false;
+                    rollSlerp = rollSpeed;
+                }
+                else
+                {
+                    Roll();
+                }
+            }
+        }
 
         public void MoveHorizontal(int dir, float ratio = 1f)
         {
@@ -31,6 +54,18 @@ namespace Assets.Scripts.Player
             {
                 transform.Translate(Vector3.up * moveSpeed * Time.deltaTime * ratio);
             }
+        }
+
+        public void InitRoll()
+        {
+            rolling = true;
+        }
+        private void Roll()
+        {
+            if(facingRight)
+                transform.Translate(Vector3.right * rollSlerp * Time.deltaTime);
+            else
+                transform.Translate(Vector3.left * rollSlerp * Time.deltaTime);
         }
 
         public bool FacingRight
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,11 +13,12 @@ namespace Assets.Scripts.Player
         {
             float hor = InputManager.GetAxis("Horizontal_P" + (int)id, id);
             float vert = InputManager.GetAxis("Vertical_P" + (int)id, id);
+
             if (hor > 0)
             {
                 movement.MoveHorizontal(1, Mathf.Abs(hor));
             }
-            if (hor < 0)
+            else if (hor < 0)
             {
                 movement.MoveHorizontal(-1, Mathf.Abs(hor));
             }
@@ -25,9 +26,14 @@ namespace Assets.Scripts.Player
             {
                 movement.MoveVertical(1, Mathf.Abs(vert));
             }
-            if (vert < 0)
+            else if (vert < 0)
             {
                 movement.MoveVertical(-1, Mathf.Abs(vert));
+            }
+
+            if(InputManager.GetButtonDown("B_P" + (int)id, id))
+            {
+                movement.InitRoll();
             }
         }
     }
