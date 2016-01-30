531d5594a6cafe8cdbbf3bc50b198eb33eee4120
Eric Cook <jcook83@gatech.edu>
2016-01-30 01:21:07 -0500

    Rotation in rolling

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -10,6 +10,7 @@ namespace Assets.Scripts.Player
         private float rollTimer = 0, rollTime = 0.3f;
         private float moveSpeed = 5f;
         private float rollSpeed = 20f, rollSlerp = 20f, targetSlerp = 5f, slerpSpeed = 0.1f;
+        private float rotationSpeed = 0.1f, rotationVel = 0f, targetZRotation = 360, currentZRotation = 0f;
         private float rollVel = 0f;
 
         void Update()
@@ -23,6 +24,8 @@ namespace Assets.Scripts.Player
                     rollTimer = 0;
                     rolling = false;
                     rollSlerp = rollSpeed;
+                    currentZRotation = 0f;
+                    transform.rotation = Quaternion.Euler(0, 0, currentZRotation);
                 }
                 else
                 {
@@ -35,12 +38,12 @@ namespace Assets.Scripts.Player
         {
             if (dir < 0)
             {
-                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime * ratio);
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime * ratio, Space.World);
                 facingRight = false;
             }
             else
             {
-                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime * ratio);
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime * ratio, Space.World);
                 facingRight = true;
             }
         }
@@ -48,24 +51,31 @@ namespace Assets.Scripts.Player
         {
             if (dir < 0)
             {
-                transform.Translate(Vector3.down * moveSpeed * Time.deltaTime * ratio);
+                transform.Translate(Vector3.down * moveSpeed * Time.deltaTime * ratio, Space.World);
             }
             else
             {
-                transform.Translate(Vector3.up * moveSpeed * Time.deltaTime * ratio);
+                transform.Translate(Vector3.up * moveSpeed * Time.deltaTime * ratio, Space.World);
             }
         }
 
         public void InitRoll()
         {
             rolling = true;
+            if (facingRight)
+                targetZRotation = -360f;
+            else
+                targetZRotation = 360f;
         }
         private void Roll()
         {
             if(facingRight)
-                transform.Translate(Vector3.right * rollSlerp * Time.deltaTime);
+                transform.Translate(Vector3.right * rollSlerp * Time.deltaTime, Space.World);
             else
-                transform.Translate(Vector3.left * rollSlerp * Time.deltaTime);
+                transform.Translate(Vector3.left * rollSlerp * Time.deltaTime, Space.World);
+
+            currentZRotation = Mathf.SmoothDamp(currentZRotation, targetZRotation, ref rotationVel, rotationSpeed);
+            transform.rotation = Quaternion.Euler(0, 0, currentZRotation);
         }
 
         public bool FacingRight
